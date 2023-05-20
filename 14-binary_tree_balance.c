534fca7d58884553912982d66c174e1777ca9d78
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 16:39:59 -0700

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	lh = binary_tree_balance(tree->left);
-	rh = binary_tree_balance(tree->right);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 
 	return (lh - rh);
 }
