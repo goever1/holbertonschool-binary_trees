f45acdbd73ee54975dd2994fff46b39dbaa765ef
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 09:47:59 -0700

    FIX 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,7 @@
  * @value: value
  * Return: binary tree node
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
