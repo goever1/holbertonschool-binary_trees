a0ad01f4890ce8d5ccd61b2b080e1443442412de
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 15:38:51 -0700

    BT leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - it finds out the amount of leaves in the tree
+ * @tree: it is a pointer to the root of the binary tree
+ * Return: the amount of leaves in the tree or 0 if error
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+	if(!tree->right && !tree->left)
+		leaves += 1;
+
+	binary_tree_leaves(tree->rigth);
+	binary_tree_leaves(tree->left);
+	
+	return (leaves);
+}
