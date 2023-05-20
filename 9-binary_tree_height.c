91c554db3d017d5285125be70d6ba6c3611f298e
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 12:41:35 -0700

    BT height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,12 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int lh = 0, rh = 0;
-		
+	
 	if (!tree)
 		return (0);
 	if (tree->left)
 		lh = 1 + binary_tree_height(tree->left);
 	if (tree->right)
 		rh = 1 + binary_tree_height(tree->right);
-	return (lh > rh) ? lh : rh;
+	return ((lh > rh) ? lh : rh);
 }
