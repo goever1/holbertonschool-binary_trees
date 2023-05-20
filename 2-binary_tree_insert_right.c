0ac44a11f1be1d3b9d671698ad51ac8f57320085
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 10:55:54 -0700

    inster node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node
+ * @parent: parent node
+ * @value: value to store in the new node
+ * Return: the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);
+	if (!node)
+		return (NULL);
+	if (parent->right)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
+
+}
