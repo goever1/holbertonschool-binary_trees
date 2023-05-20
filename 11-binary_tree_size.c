34e0e6c8584527a59f2512472e6f69f8936645ba
root <jorgefernandoochoa1@hotmail.com>
2023-05-20 15:17:23 -0700

    BT size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,15 +8,13 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t lh = 0, rh = 0;
+	size_t size = 0;
 
 	if(!tree)
 		return (0);
-	if(tree->left)
-		lh = 1 + binary_tree_size(tree->left);
-	if(tree->right)
-		rh = 1 + binary_tree_size(tree->right);
+
+	size = 1 + binary_tree_size(tree->right) + binary_tree_size(tree->right);
 	
-	return (lh + rh + 1);
+	return (size);
 
 }
