45f3b4d58cff9c9daa5eb22bed44e7ce07a1fbe4
root <jorgefernandoochoa1@hotmail.com>
2023-05-21 08:43:56 -0700

    balance point

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,10 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		lh += binary_tree_balance(tree->left);
-	if (tree->right)
-		rh += binary_tree_balance(tree->right);
+	lh = 1 + binary_tree_balance(tree->left);
+	rh = 1 + binary_tree_balance(tree->right);
 
-	return (rh - lh);
+	return (lh - rh);
 }
