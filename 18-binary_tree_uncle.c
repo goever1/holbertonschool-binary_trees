1f0bbc0d9450772dfc80c058538cd0d5b7549216
root <jorgefernandoochoa1@hotmail.com>
2023-05-21 11:45:56 -0700

    uncle node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 	if (node->parent->parent->left == node->parent && node->parent->parent->right)
 		return (node->parent->parent->right);
