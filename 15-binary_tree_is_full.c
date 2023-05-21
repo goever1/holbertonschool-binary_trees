2ee2bbb5585e2c3796599b4396b4472b6d38a05e
root <jorgefernandoochoa1@hotmail.com>
2023-05-21 09:17:01 -0700

    BT full check

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 	if (tree->right && tree->left)
-		return (binary_tree_leaves(tree->right) && binary_tree_leaves(tree->left));
+		return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
 
 	return (0);
 }
