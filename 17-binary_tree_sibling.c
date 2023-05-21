de5e3e29e9348712efd3e0116f7bd0b4616224ce
root <jorgefernandoochoa1@hotmail.com>
2023-05-21 11:31:18 -0700

    sibling node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if(!node->parent->left || !node->parent->right)
-		return(NULL);
-	if(node->parent->left == node)
+	if(node->parent->left == node && node->parent->right)
 		return (node->parent->right);
-	if(node->parent->right == node)
+	if(node->parent->right == node && node->parent->left)
 		return (node->parent->left);
+	return (NULL);
 }
