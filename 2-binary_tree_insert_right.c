1a93170f4408cfacdbaf06046e6864746980fdce
goever1 <30449167+goever1@users.noreply.github.com>
2023-05-21 13:50:33 -0500

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,10 +8,11 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node
 
 	if (!parent)
 		return (NULL);
+	node = binary_tree_node(parent, value);
 	if (!node)
 		return (NULL);
 	if (parent->right)
